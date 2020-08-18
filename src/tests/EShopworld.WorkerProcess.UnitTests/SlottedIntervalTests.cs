50fca84ad5bfef881c44ce846bd08fba1183720f
Antonino Parisi <aparisi@eshopworld.com>
2020-08-18 11:05:12 +0200

    Improve SlottedInterval tests

--- src/tests/EShopworld.WorkerProcess.UnitTests/SlottedIntervalTests.cs
+++ src/tests/EShopworld.WorkerProcess.UnitTests/SlottedIntervalTests.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
 using Eshopworld.Tests.Core;
-using EShopworld.WorkerProcess.Infrastructure;
 using FluentAssertions;
 using Xunit;
 
@@ -17,24 +17,36 @@ namespace EShopworld.WorkerProcess.UnitTests
         }
 
         [Theory, IsUnit]
-        [InlineData(637333444195566768L, 60000, 443.3231999996642)]     // 1 min
-        [InlineData(637333444195566768L, 300000, 180443.3232000065)]    // 5 min
-        [InlineData(637333444195566768L, 900000, 780443.3232000001)]    // 15 min
-        [InlineData(637333444195566768L, 3600000, 780443.3232)]         // 1 hour
-        [InlineData(630823248000000000L, 60000, 60000)]                 // 1 min
-        [InlineData(630823248000000000L, 300000, 300000)]               // 5 min
-        [InlineData(630823248000000000L, 900000, 900000)]               // 15 min
-        [InlineData(630823248000000000L, 3600000, 3600000)]             // 1 hour
-        public void TestInterval(long ticks, double interval, double expectedSlottedInterval)
+        [MemberData(nameof(Data))]
+        public void TestInterval(DateTime now, TimeSpan interval, double expectedSlottedInterval)
         {
             // Arrange
-            var now = new DateTime(ticks, DateTimeKind.Utc);
 
             // Act
-            var result = _slottedInterval.Calculate(now, TimeSpan.FromMilliseconds(interval));
+            var result = _slottedInterval.Calculate(now, interval);
 
             // Assert
             result.TotalMilliseconds.Should().BeApproximately(expectedSlottedInterval, _precision);
         }
+
+        private static DateTime OnTheHour = new DateTime(2000, 1, 1, 12, 0, 0);
+        private static DateTime NotOnTheHour = new DateTime(2000, 1, 1, 11, 20, 10, 255);
+        private static TimeSpan Interval_1Min = TimeSpan.FromMinutes(1);
+        private static TimeSpan Interval_5Min = TimeSpan.FromMinutes(5);
+        private static TimeSpan Interval_12Min = TimeSpan.FromMinutes(12);
+        private static TimeSpan Interval_1Hour = TimeSpan.FromHours(1);
+
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                new object[] { NotOnTheHour, Interval_1Min, 49744.99999999807 },
+                new object[] { NotOnTheHour, Interval_5Min, 289744.9999999964 },
+                new object[] { NotOnTheHour, Interval_12Min, 229744.99999999808 },
+                new object[] { NotOnTheHour, Interval_1Hour, 2389745.000000001 },
+                new object[] { OnTheHour, Interval_1Min, Interval_1Min.TotalMilliseconds },
+                new object[] { OnTheHour, Interval_5Min, Interval_5Min.TotalMilliseconds },
+                new object[] { OnTheHour, Interval_12Min, Interval_12Min.TotalMilliseconds },
+                new object[] { OnTheHour, Interval_1Hour, Interval_1Hour.TotalMilliseconds },
+            };
     }
 }
