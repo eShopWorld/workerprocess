377c676564523ed706c6470a7014cd3bc6975b8b
Ferdinando Aprovitolo <faprovitolo@eshopworld.com>
2020-12-08 09:53:12 +0000

    Added ConfigureAwait(false) in ExecuteIn method in SystemTimer

--- src/Eshopworld.WorkerProcess/Infrastructure/SystemTimer.cs
+++ src/Eshopworld.WorkerProcess/Infrastructure/SystemTimer.cs
@@ -29,7 +29,7 @@ namespace EShopworld.WorkerProcess.Infrastructure
                 {
                     await executor();
                 }
-            });
+            }).ConfigureAwait(false);
         }
 
         /// <inheritdoc />
