51b666152de57a6001d19be83710e405bf263879
Nikhil Khandelwal <nkhandelwal@eshopworld.com>
2021-01-11 16:40:56 +0000

    Added DistributedLock draft 2

---
+++ src/Eshopworld.WorkerProcess/DistributedLock/IDistributedLock.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EShopworld.WorkerProcess.DistributedLock
+{
+    public interface IDistributedLock : IDisposable
+    {
+        public Task<IDisposable> Acquire(string requestJobName);
+    }
+}
